a2b4c173e32fd5a2597dba0ccb47c414a03a6016
Zhengzhong Shi <mis481ecommerce@gmail.com>
2023-01-16 16:56:04 -0500

    hello.py

---
+++ 01_hello/hello.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""
+Author : zshi <zshi@localhost>
+Date   : 2023-01-16
+Purpose: Say Hello
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='hello project',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-n',
+                        '--name',
+                        help='A named string argument',
+                        metavar='name',
+                        type=str,
+                        default='World')
+
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    word= args.name
+    print(f"Hello, {word}!")
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
